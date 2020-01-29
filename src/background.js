b0e807ffb7e3c766ad928887b0840c76a2ba85dc
Luca <sarius009@gmail.com>
2020-01-29 22:36:59 +0100

    Add background script

---
+++ src/background.js
@@ -0,0 +1,17 @@
+const pageStateMatcher = new chrome.declarativeContent.PageStateMatcher({
+  pageUrl: {
+    hostEquals: 'amazon.com',
+  },
+});
+
+chrome.runtime.onInstalled.addListener((details) => {
+  console.log(`Added listener. (${details.reason})`);
+  if (details.reason === 'update') console.log(`Previous version: "${details.previousVersion}"`);
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [pageStateMatcher],
+      actions: [new chrome.declarativeContent.ShowPageAction()],
+    }]);
+  });
+});
