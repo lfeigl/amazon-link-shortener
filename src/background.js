647e1cf53906ef689a3537d28e1a27aca4406948
Luca <sarius009@gmail.com>
2020-01-30 21:41:06 +0100

    Add url matcher & tlds

--- src/background.js
+++ src/background.js
@@ -1,6 +1,44 @@
+// Top-level domains (TLDs)
+const tlds = [
+  // United Arab Emirates
+  'ae',
+  // Canada
+  'ca',
+  // China
+  'cn',
+  // Japan
+  'co.jp',
+  // United Kingdom
+  'co.uk',
+  // United States
+  'com',
+  // Australia
+  'com.au',
+  // Brazil
+  'com.br',
+  // Mexico
+  'com.mx',
+  // Turkey
+  'com.tr',
+  // Germany
+  'de',
+  // Spain
+  'es',
+  // France
+  'fr',
+  // Italy
+  'it',
+  // India
+  'in',
+  // Netherlands
+  'nl',
+  // Singapore
+  'sg',
+].join('|');
+
 const pageStateMatcher = new chrome.declarativeContent.PageStateMatcher({
   pageUrl: {
-    hostEquals: 'www.amazon.com',
+    urlMatches: `^http[s]?://www.amazon.(${tlds})[\\w\\W]*(/dp/|/product/)`,
   },
 });
 
